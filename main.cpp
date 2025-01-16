cdc10b4c8ea2f33bf3d090ed48c5189272a9a328
s.toshtemirov <s.toshtemirov@newuu.uz>
2025-01-16 14:42:05 +0500

    Nothing yet

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Hello World!" << endl;
+    int age;
+    cout << "Please enter your age: ";
+    cin >> age;
+    cout <<"According to my information, you are "<< age << endl;
+    return 0;
+
+}
