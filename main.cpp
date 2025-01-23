20071071941dd514519948ae8f72fc7a8073f27e
s.toshtemirov <s.toshtemirov@newuu.uz>
2025-01-23 15:17:22 +0500

    5 tasks now

--- main.cpp
+++ main.cpp
@@ -1,11 +1,48 @@
 #include <iostream>
+#include <iomanip>
+#include <string>
 using namespace std;
 int main() {
-    cout << "Hello World!" << endl;
-    int age;
-    cout << "Please enter your age: ";
-    cin >> age;
-    cout <<"According to my information, you are "<< age << endl;
-    return 0;
-
+    //First task:
+        // cout << "Hello World!" << endl;
+        // string name;
+        // cout << "Enter your name: " << endl;
+        // cin >> name;
+        // int age;
+        // cout << "Please enter your age: ";
+        // cin >> age;
+        // cout <<"According to my information, you are "<< age << ", " << name << "!" <<endl;
+        // return 0;
+    //Second task:
+        // for (int i = 1; i <= 5; ++i) {
+        //     cout << setw(6) << i << setw(6);
+        // }
+        // return 0;
+    //Third task:
+        // int n;
+        // int m;
+        // cout << "Enter first number: ";
+        // cin >> n;
+        // cout << "Enter second number: ";
+        // cin >> m;
+        // cout << "Your numbers swapped: " << m << " " << n << endl;
+    //Fourth task:
+        // int n;
+        // cout << "Enter a number: " << endl;
+        // cin >> n;
+        // for (int i = 1; i <= n; ++i) {
+        //          cout << setw(2) << i << setw(6) << i*i << endl;
+        // }
+    //Fifth task:
+        // string input;
+        // cout << "Enter a string: ";
+        // getline(cin, input);
+        //
+        // int m = 50;
+        // int stringLength = input.length();
+        // int margin = (m - stringLength)/2;
+        //
+        // cout << setw(margin) << input << setw(margin) << endl;
+        //
+        // return 0;
 }
